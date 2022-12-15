41155f9daa3686fff81faaa1cebab2714a77be3a
torako-san <100730549+torako-san@users.noreply.github.com>
2022-12-15 11:34:38 +0900

    登録フォームへ遷移、登録する処理を記述

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -2,8 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.model.Calendar;
 import com.example.demo.repository.CalendarRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,5 +23,19 @@ public class IndexController {
 	public String index(Model model) {
 		model.addAttribute("Calendar", repository.findAll());
 		return "index";
+	}
+	
+	@GetMapping("/add")
+	public String addCalendar(@ModelAttribute Calendar calender) {
+		return "add";
+	}
+	
+	@PostMapping("/schedule")
+	public String postCalendar(@Validated @ModelAttribute Calendar calendar, BindingResult result) {
+		if (result.hasErrors()) {
+			return "add";
+		}
+		repository.save(calendar);
+		return "redirect:/";
 	}
 }
